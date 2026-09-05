2ebe1ee4a13a7bc3027efac8445247cbc33db3c9
Tanvir <anjumtanvirmdabir.81@gmail.com>
2026-09-05 09:14:12 +0600

    fix: api test runner script

--- tools/apitest.sh
+++ tools/apitest.sh
@@ -8,10 +8,23 @@ tmp=/tmp/filapi.apitest.$$
 
 cc="${CC:-cc}"
 
-# objects for filapi tests (exclude main.o which has main())
+# Updated object paths matching subdirectories in bin/
 objs=""
-for o in $objdir/util.o $objdir/parse.o $objdir/abi.o $objdir/cfg.o $objdir/mem.o $objdir/ssa.o $objdir/alias.o $objdir/load.o $objdir/copy.o $objdir/fold.o $objdir/gvn.o $objdir/gcm.o $objdir/simpl.o $objdir/ifopt.o $objdir/live.o $objdir/spill.o $objdir/rega.o $objdir/emit.o $objdir/amd64/targ.o $objdir/amd64/sysv.o $objdir/amd64/isel.o $objdir/amd64/emit.o $objdir/amd64/winabi.o $objdir/arm64/targ.o $objdir/arm64/abi.o $objdir/arm64/isel.o $objdir/arm64/emit.o $objdir/rv64/targ.o $objdir/rv64/abi.o $objdir/rv64/isel.o $objdir/rv64/emit.o $objdir/filapi/src/ilbuilder.o; do
-  if test -f $o; then objs="$objs $o"; fi
+for o in \
+  $objdir/util/util.o $objdir/util/parse.o \
+  $objdir/core/cfg.o $objdir/core/mem.o $objdir/core/ssa.o $objdir/core/alias.o $objdir/core/load.o $objdir/core/copy.o \
+  $objdir/opt/fold.o $objdir/opt/gvn.o $objdir/opt/gcm.o $objdir/opt/simpl.o $objdir/opt/ifopt.o \
+  $objdir/reg/live.o $objdir/reg/spill.o $objdir/reg/rega.o \
+  $objdir/emit/emit.o $objdir/emit/abi.o \
+  $objdir/amd64/targ.o $objdir/amd64/sysv.o $objdir/amd64/isel.o $objdir/amd64/emit.o $objdir/amd64/winabi.o \
+  $objdir/arm64/targ.o $objdir/arm64/abi.o $objdir/arm64/isel.o $objdir/arm64/emit.o \
+  $objdir/rv64/targ.o $objdir/rv64/abi.o $objdir/rv64/isel.o $objdir/rv64/emit.o \
+  $objdir/filapi/src/ilbuilder.o; do
+  if test -f "$o"; then
+    objs="$objs $o"
+  else
+    echo "Warning: object file $o not found" >&2
+  fi
 done
 
 cleanup(){ rm -f $tmp.exe $tmp.out; }
